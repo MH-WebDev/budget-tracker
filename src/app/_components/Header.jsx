04c6ec9fcc73cfb1b5c14d30bb71b0d7dd32a1de
MartinH <145781652+MH-WebDev@users.noreply.github.com>
2025-05-10 10:17:37 +0100

    fix: Fixed welcome page buttons, seriousified CTA

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -10,12 +10,8 @@ function Header() {
     <div className="p-5 bg-purple-700 text-white flex flex-row justify-end items-center shadow-md">
       <SignedOut>
         <div className="flex flex-row gap-5">
-          <div className="h-10 w-24 bg-accent rounded-lg text-primary flex items-center justify-center font-semibold cursor-pointer shadow-sm hover:shadow-md hover:scale-105 transition-all duration-200 ease-in-out">
-            <SignInButton className="cursor-pointer"/>
-          </div>
-          <div className="h-10 w-24 bg-primary rounded-lg text-accent flex items-center justify-center font-semibold cursor-pointer shadow-sm hover:shadow-md hover:scale-105 transition-all duration-200 ease-in-out">
-            <SignUpButton />
-          </div>
+            <SignInButton className="h-10 w-24 bg-accent rounded-lg text-primary flex items-center justify-center font-semibold cursor-pointer shadow-sm hover:shadow-md hover:scale-105 transition-all duration-200 ease-in-out"/>
+            <SignUpButton  className="h-10 w-24 bg-primary rounded-lg text-accent flex items-center justify-center font-semibold cursor-pointer shadow-sm hover:shadow-md hover:scale-105 transition-all duration-200 ease-in-out"/>
         </div>
       </SignedOut>
       <SignedIn>
